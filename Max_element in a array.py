efe427e3de65f3da4cb974fdd54250072c813d9e
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-24 20:19:09 +0530

    Add files via upload

---
+++ Max_element in a array.py
@@ -0,0 +1,9 @@
+print("Enter the size of the array:")
+n=int(input())
+print("Enter ",n,"values:")
+l=list(map(int,input().split()))
+max_ele=l[0]
+for i in range(1,n):
+    if l[i]>max_ele:
+        max_ele=l[i]
+print("The maximum element in the array :",max_ele)
