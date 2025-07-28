8d6a905f3b366c31e5dcacc35ad902561ce95a74
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-28 23:35:13 +0530

    Add files via upload

---
+++ Pattern printing.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    cout<<"Enter number of rows:"<<endl;
+    cin>>rows;
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=rows;j>0;j--)
+        {
+            if (i!=j)
+            {
+            cout<<j<<"";
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
