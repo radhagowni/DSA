f5b07b6d86f05eff86c840c2a588333647a598cf
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-12 16:47:40 +0530

    Add files via upload

---
+++ Fibonacci_approch_3.py
@@ -0,0 +1,17 @@
+#if index starts from 0 and fibonacci series starts from 0
+def fibonacci(n):
+    l=[]
+    a=0 
+    b=1 
+    for i in range(1,n):
+        if i>1:
+           c=a+b 
+           a=b 
+           b=c
+           l.append(c) 
+        else:
+            l.append(a)
+            l.append(b)
+    return l[n-1]
+n=int(input("Enter the value:"))
+print(fibonacci(n))
