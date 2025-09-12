1bd04e8899e7b0dbb461db5fbb33b868ec12721d
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-12 16:47:56 +0530

    Add files via upload

---
+++ Fibonacci_approch_2.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    l=[]
+    a=1 
+    b=1 
+    for i in range(1,n):
+        if i>1:
+           c=a+b 
+           a=b 
+           b=c
+           l.append(c) 
+        else:
+            l.append(a)
+            l.append(b)
+    return l[n-1]
+n=int(input("Enter the value:"))
+print(fibonacci(n))
