9fe6e2d6c7d572a16be3f7d50bcfe0ffdeb52285
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-13 17:44:15 +0530

    Initial commit

---
+++ Reverse of a string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string st;
+    cin>>st;
+    for(int i=(strlen(st))-1;i>=0;i--)
+    {
+        cout<<st[i];
+    }
+}
