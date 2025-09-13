40c0b06d35666989e932a99e588b069aff392ea5
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-13 17:31:46 +0530

    Add files via upload

---
+++ Max_and_min element in an array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int max,min;
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    min=a[0];
+    for(int i=1;i<n;i++)
+    {
+        if (a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    max=a[0];
+    for(int i=1;i<n;i++)
+    {
+        if (a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    cout<<min<<" "<<max;
+    
+}
