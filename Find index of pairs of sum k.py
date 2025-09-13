8918cb151d511b3b125486c0ca3d1488267bfc2f
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-13 17:28:37 +0530

    Initial commit

---
+++ Find index of pairs of sum k.py
@@ -0,0 +1,15 @@
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+f=0
+r=n-1
+while(f<r):
+    s=a[f]+a[r]
+    if s==k:
+        print(f,r)
+        break
+    elif s<k:
+        f=f+1 
+    else:
+        r=r-1 
+else:
+    print(-1)
