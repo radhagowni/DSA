8f17f77b183f66e01b208766f6f1d360d7b837f0
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-14 11:36:56 +0530

    Initial commit

---
+++ Sum of the queries in the given range.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,queries;
+    int first,last;
+    cin>>n;
+    int a[n];
+    int s[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    s[0]=a[0];
+    for(int i=1;i<n;i++)
+    {
+       s[i]=a[i]+s[i-1];
+    }
+    
+    
+    cin>>queries;
+    for(int j=0;j<queries;j++)
+    {
+       cin>>first>>last; 
+   
+    if(first==0)
+    {
+        cout<<s[last];
+    }
+    else{
+        cout<<s[last]-s[first-1];
+    }
+    }
+}
