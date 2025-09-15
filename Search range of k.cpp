edd963abbe258ef98a16005e4946a8b8f19a2cca
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-15 16:27:46 +0530

    Initial commit

---
+++ Search range of k.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int n,k,idx,idx1;
+    cin>>n;
+    int a[n];
+    idx1=idx=-1;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cin>>k;
+    for(int i=0;i<n;i++)
+    {
+        if (a[i]==k)
+        {
+            idx=i;
+            break;
+        }
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        if(a[i]==k)
+        {
+            idx1=i;
+            break;
+        }
+    }
+    cout<<idx<<" "<<idx1;
+    
+ 
+}
