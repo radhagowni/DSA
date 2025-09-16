fcda865b3f2e2df6a3ca9843e2361f5769dc2002
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-16 16:32:37 +0530

    Initial commit

---
+++ Exclusion product_approch2.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+mod=(10**9)+7
+res=[1]*n
+left=1
+for i in range(n):
+    res[i]=left
+    left=(left*arr[i])%mod
+right=1
+for i in range(n-1,-1,-1):
+    res[i]=(res[i]*right)%mod
+    right=(right*arr[i])%mod
+print(*res)
