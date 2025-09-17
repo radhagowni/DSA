db7a7bbaeb5b86bff0ef20ffaf85fa10ecba1bfd
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-17 18:48:02 +0530

    Initial commit

---
+++ Perfect number_approach2.py
@@ -0,0 +1,16 @@
+num=int(input())
+if num < 2: 
+    print(False) 
+else:
+        s = 1
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                s += i
+                if i * i != num:
+                    s += num // i
+            i += 1
+if  s == num:
+    print(True)
+else:
+    print(False)
