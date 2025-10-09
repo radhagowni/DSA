f1b4d7262d34b9d5ac97f6adc4059028ba6458b6
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-10-09 16:50:05 +0530

    Add files via upload

---
+++ Super power.py
@@ -0,0 +1,12 @@
+def superPow(a, b):
+
+    MOD = 1337
+    ans = 1
+
+    for i in b:
+      ans = pow(ans, 10, MOD) * pow(a, i, MOD)
+
+    return ans % MOD
+a=int(input())
+b=list(map(int,input().split()))
+print(superPow(a,b))
