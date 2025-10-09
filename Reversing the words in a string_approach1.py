577872ae4aa0528e9c9ead036af75306eeba9962
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-10-09 16:48:29 +0530

    Initial commit

---
+++ Reversing the words in a string_approach1.py
@@ -0,0 +1,14 @@
+def reverseWords(s):
+    s=s.strip()
+    ar=list(map(str,s.split(" ")))
+    t=''
+    for i in range(len(ar)-1,-1,-1):
+      if ar[i]=='':
+        continue
+      else:
+         t+=ar[i]
+         if i!=0:
+           t=t+" "
+    return  '"'+t+'"'
+s=input()
+print(reverseWords(s))
